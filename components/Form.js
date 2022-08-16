e2861ed65d22a03a11d854832e7c99224d4a1842
salihayazdir <salihayazdir@gmail.com>
2022-08-16 16:42:58 +0300

    initial commit

---
+++ components/Form.js
@@ -0,0 +1,43 @@
+
+export default function Form({formData, setFormData}) {
+
+    const handleChange = (e) => {
+        setFormData(
+          {...formData,
+            [e.target.name]: e.target.value}
+        )
+      }
+    
+    const handleSubmit =  (e) => {
+        e.preventDefault();
+        console.log(formData)
+    }
+        
+    const fieldStyles = 'focus:bg-slate-100 focus:outline-0 px-6 py-4 mb-[-px] border-b-2 border-black'
+    
+    return (
+     <form 
+        name="form" onSubmit={handleSubmit}
+        className=''>
+            <input
+                id='name' type='text' name='name' placeholder='Your Name'
+                onChange={handleChange} value={formData.name}
+                className={`${fieldStyles}`}
+            />
+            <input
+                id='email' type='email' name='email' placeholder='Your E-mail Adress'
+                onChange={handleChange} value={formData.email}
+                className={`${fieldStyles}`}
+            />
+            <input
+                id='phone' type='text' name='phone' placeholder='Your phone'
+                onChange={handleChange} value={formData.phone}
+                className={`${fieldStyles}`}
+            />
+            <button type='submit'
+                className='px-6 py-4 text-left text-2xl font-bold outline outline-2 outline-black hover:bg-black hover:text-white'
+                >SEND
+            </button>
+        </form>
+  )
+}
