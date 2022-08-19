f9595ad0b1fe44d9c326308b7dce46ebf1d2d7f1
salihayazdir <salihayazdir@gmail.com>
2022-08-19 13:40:09 +0300

    pwa config2

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-/** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa");
+/** @type {import('next').NextConfig} */
 
 const nextConfig = withPWA({
   pwa: {
     dest: "public",
     register: true,
     swSrc: 'service-worker.js',
-    // disable: process.env.NODE_ENV === 'development'
+    disable: process.env.NODE_ENV === 'development'
   },
   reactStrictMode: true,
   swcMinify: true,
